ab48353546a7e48406389814acecbad1b2c3b1ee
hassenH <hadhri-h@live.fr>
2019-02-09 15:28:48 +0100

    Ajout Exercice 2

---
+++ assets/js/javascript.js
@@ -0,0 +1,8 @@
+function display(){
+//Pour masquer la division :
+document.getElementById('text').style.visibility = "visible";
+}
+function hide(){
+///Pour afficher la division :
+document.getElementById('text').style.visibility = "hidden";
+}
